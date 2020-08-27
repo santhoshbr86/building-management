1926118cc4e0f0f68fb86135dc2f0e58cb86f36f
santhoshbr86 <santhoshbr86@gmail.com>
2020-08-27 13:32:02 +0530

    dist folder changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/building-managment'));
+app.use(express.static(__dirname + '/dist/browser'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/building-managment/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
